5391a94e9c1f284d282d91ea610876dfa2c88c5e
Shafiraaa25 <firra251201@gmail.com>
2025-03-21 10:58:36 +0700

    .env.example

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\BoardsController;
 use App\Http\Controllers\TasksController;
-<<<<<<< HEAD
-use App\Http\Controllers\ContactController; // Tambahkan ini jika Anda menggunakan ContactController
-=======
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ContactController;
->>>>>>> aa77acd26f26d2e6d970a3ad5d25f8beb4371ad5
 
 /*
 |--------------------------------------------------------------------------
@@ -28,22 +24,22 @@ Route::get('/', function () {
 
 Auth::routes();
 
+// Route untuk mengirim data kontak
 Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-// Route untuk mengirim data kontak (contact.submit)
-Route::post('/contact/submit', [ContactController::class, 'submit'])->name('contact.submit'); // Gunakan ContactController dan method submit
-
+// Group middleware untuk admin dengan prefix "admin"
 Route::group(['middleware' => 'isAdmin', 'prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::get('dashboard', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard.index');
     Route::resource('permissions', \App\Http\Controllers\Admin\PermissionController::class);
     Route::resource('roles', \App\Http\Controllers\Admin\RoleController::class);
     Route::resource('users', \App\Http\Controllers\Admin\UserController::class);
+
+    // Profil admin
     Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
     Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
 });
 
+// Group middleware untuk pengguna yang sudah login
 Route::middleware(['auth'])->group(function () {
     Route::resource('boards', BoardsController::class);
     Route::get('/tasks/{board}', [TasksController::class, 'index'])->name('tasks.index');
